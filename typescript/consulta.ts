1354521cb30c4dac2d0fc06b14464f09877286ad
Giovistica <giovi.rhcp@gmail.com>
2023-07-31 20:26:36 -0300

    clase de consulta typescript

---
+++ typescript/consulta.ts
@@ -0,0 +1,8 @@
+/**
+ * Crear una regla para el ingreso de un producto, en donde;
+ *
+ * producto tenga un nombre, un precio y el stock.
+ *
+ * las condiciones de la reglas son:
+ * que tenga todos los campos, que el precio sea positivo y el stock tiene que ser un entero tambien
+ */
